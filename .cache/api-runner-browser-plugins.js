5e7bc18d0a6e154dfba63b3129750427b7e37a91
nox <noxhsxrk@icloud.com>
2024-07-03 21:52:56 +0700

    feat: first commit

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,16 @@
+module.exports = [{
+      plugin: require('../node_modules/.pnpm/gatsby-remark-images@7.13.1_gatsby-plugin-sharp@5.13.1_gatsby@5.13.6_@types+webpack@5.28.5_ba_62ebp3cz2sfq6j2m35o7h7j4ye/node_modules/gatsby-remark-images/gatsby-browser.js'),
+      options: {"plugins":[],"maxWidth":960,"quality":90,"linkImagesToOriginal":false,"backgroundColor":"transparent","showCaptions":false,"markdownCaptions":false,"withWebp":false,"withAvif":false,"loading":"lazy","decoding":"async","disableBgImageOnAlpha":false,"disableBgImage":false},
+    },{
+      plugin: require('../node_modules/.pnpm/gatsby-plugin-catch-links@5.13.1_gatsby@5.13.6_@types+webpack@5.28.5_babel-eslint@10.1.0_esli_5ia75yq4rw7miue3hktdxv7ctq/node_modules/gatsby-plugin-catch-links/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../node_modules/.pnpm/gatsby-plugin-theme-ui@0.15.7_@emotion+react@11.11.4_@types+react@18.3.3_react@18.3.1__@mdx-j_bsqo2fhh5ew22ta6yjy5pp7npq/node_modules/gatsby-plugin-theme-ui/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../node_modules/.pnpm/gatsby-plugin-manifest@5.13.1_gatsby@5.13.6_@types+webpack@5.28.5_babel-eslint@10.1.0_eslint@_r54papxovyw5fwjqofi345neza/node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"Nox's Personal Dev Blog","short_name":"Nox's Personal Dev Blog","description":"Nox's Personal Dev Blog","start_url":"/","background_color":"#fff","display":"standalone","icons":[{"src":"/android-chrome-192x192.png","sizes":"192x192","type":"image/png"},{"src":"/android-chrome-512x512.png","sizes":"512x512","type":"image/png"}],"legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":null},
+    },{
+      plugin: require('../node_modules/.pnpm/gatsby@5.13.6_@types+webpack@5.28.5_babel-eslint@10.1.0_eslint@7.32.0__react-dom@18.3.1_react_5zuxzdpefhnx3jlgdhjb6ajuti/node_modules/gatsby/dist/internal-plugins/partytown/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
